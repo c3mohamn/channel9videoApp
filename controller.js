6cba03a053cab93a53eb915482fe3764ec1ab837
Nasir M <nasir.m23@hotmail.com>
2017-06-30 05:13:07 -0400

    Basic information displayed.

--- controller.js
+++ controller.js
@@ -1,25 +1,30 @@
-var videoApp = angular.module('videoApp', []);
-
-videoApp.config(['$httpProvider', function($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-]);
+var videoApp = angular.module('videoApp', ['ngSanitize']);
 
 videoApp.controller('videoCtrl', ['$scope', '$http', function($scope, $http) {
 
   $scope.rssFeed = "";
-  // TODO: get RSS feed
+  var feed = [];
 
-  //https://steinbring.net/2014/angularjs-creating-a-simple-rss-reader/
-  function getFeed(url) {
-    var jsonFeed = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'"
-    + url + "'&format=json";
+  var url = "https://channel9.msdn.com/all/rss";
+  // Yahoo api transforms xml feed into json
+  var jsonFeed = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'"
+  + url + "'&format=json";
 
-    $http.get(jsonFeed).success(function (data) {
-        $scope.rssFeed = data.query.results.rss;
-    });
-  }
+  $http.get(jsonFeed).success(function (data) {
+      feed = data.query.results.rss;
 
-  getFeed("https://channel9.msdn.com/all/rss");
+      // Loop through feed & modify iff necessary
+      angular.forEach(feed.channel.item, function(item) {
+        console.log(item);
+        //console.log(item.description);
+
+        // Removes the ending img tag that causes error for us
+        var img_loc = item.description.indexOf('<img');
+        item.description = item.description.slice(0, img_loc);
+
+        //console.log(item.description);
+      });
+
+      $scope.rssFeed = feed;
+  });
 }]);
