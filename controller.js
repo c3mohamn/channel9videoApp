a7dd9f83ffc06a89bf551c966253059879a08aaa
Nasir M <nasir.m23@hotmail.com>
2017-07-03 17:53:57 -0400

    Video Play+Duration, Simple Filters, More Visuals/Animations

--- controller.js
+++ controller.js
@@ -12,7 +12,7 @@ videoApp.config(function($sceDelegateProvider) {
    // Allow same origin resource loads.
    'self',
    // Allow loading from our assets domain..
-   'http://video.ch9.ms/ch9/**']);
+   'http://**']);
 });
 
 /* Returns Scroll Position of Page */
@@ -33,22 +33,38 @@ videoApp.controller('videoCtrl', ['$scope', '$http', function($scope, $http) {
   $scope.feed = {}; // template feed
   var feed = {      // altered feed
     title: String,
-    items: []
+    icon: String,
+    description: String,
+    items: [] // list of feed_items
   };
   var feed_item = { // values from feed items that we want
     title: String,
     author: String,
     date: String,
+    link: String,
     thumbnail: String,
     description: String,
+    duration: String,
     video: String,
     tags: []
   };
+  $scope.playVideo = false;
+  $scope.searchFilters = {
+    showFilters: false,
+    title: true,
+    author: true,
+    tags: true,
+    duration: false,
+    date: false
+  }
 
   // GET RSS feed form Channel9
   $http.get(jsonFeed).success(function (data) {
       var original_feed = data.query.results.rss;
+
       feed.title = original_feed.channel.title;
+      feed.icon = original_feed.channel.image[0].url;
+      feed.description = original_feed.channel.description;
       console.log(original_feed);
 
       // Loop through feed & modify iff necessary
@@ -60,9 +76,12 @@ videoApp.controller('videoCtrl', ['$scope', '$http', function($scope, $http) {
         feed_item.title = item.title;
         feed_item.author = item.author;
         feed_item.date = item.pubDate;
+        feed_item.link = item.link;
         feed_item.thumbnail = item.thumbnail[1].url;
         feed_item.description = item.description.slice(0, img_loc);
+        feed_item.duration = format_time(item.duration);
         if (item.group) feed_item.video = find_mp4(item.group.content);
+        else feed_item.video = null;
         // iff tag is string, convert to array
         if (angular.isString(item.category)) feed_item.tags.push(item.category);
         else feed_item.tags = item.category;
@@ -75,6 +94,8 @@ videoApp.controller('videoCtrl', ['$scope', '$http', function($scope, $http) {
 
   /* Search feed for feed item's that match search_val. */
   $scope.search_feed = function(search_val) {
+    console.log($scope.searchFilters.title);
+
 
     // If no search val, return to default
     if (!search_val) {
@@ -91,14 +112,18 @@ videoApp.controller('videoCtrl', ['$scope', '$http', function($scope, $http) {
       var matches = false;
 
       // Check if matches with item tags || title || author.
-      for (var key in item.tags) {
-        if (item.tags[key].toLowerCase().indexOf(search_val) > -1)
-          matches = true;
+      if ($scope.searchFilters.tags) {
+        for (var key in item.tags) {
+          if (item.tags[key].toLowerCase().indexOf(search_val) > -1)
+            matches = true;
+        }
       }
-      if (item.title.toLowerCase().indexOf(search_val) > -1)
-        matches = true;
-      if (item.author.toLowerCase().indexOf(search_val) > -1 )
-        matches = true;
+      if ($scope.searchFilters.title)
+        if (item.title.toLowerCase().indexOf(search_val) > -1)
+          matches = true;
+      if ($scope.searchFilters.author)
+        if (item.author.toLowerCase().indexOf(search_val) > -1 )
+          matches = true;
 
       if (matches)
         matching_items.push(item);
@@ -106,18 +131,30 @@ videoApp.controller('videoCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.feed.items = matching_items;
   }
-
-  /* Tries to find the first playable mp4 video in rss feed item's list
-   * of videos.
-   */
-  function find_mp4(videos) {
-    //console.log(videos);
-    for (var vid in videos) {
-      //console.log(vid);
-      // return if mp4 and is not filesize 1
-      if (videos[vid].fileSize > 1 && videos[vid].type == "video/mp4")
-        return videos[vid].url;
-    }
-  }
-
 }]);
+
+/* ------ Helper Functions ------ */
+
+/* Converts seconds to 00:00:00 format. */
+function format_time(seconds) {
+  // Add padding (01:01:01) instad of (1:1:1)
+  function padTime(t) { return t < 10 ? "0" + t : t; }
+
+  var hours = padTime(Math.floor(seconds / 3600)),
+      minutes = padTime(Math.floor((seconds % 3600) / 60)),
+      seconds = padTime(Math.floor(seconds % 60));
+
+  if (hours > 0) return hours + ":" + minutes + ":" + seconds;  // 00:00:00
+  else return minutes + ":" + seconds;                          // 00:00
+}
+
+/* Tries to find the first playable mp4 video in rss feed item's list
+ * of videos.
+ */
+function find_mp4(videos) {
+  for (var vid in videos) {
+    // return if mp4
+    if (videos[vid].type == "video/mp4")
+      return videos[vid].url;
+  }
+}
